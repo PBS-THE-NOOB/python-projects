7ecf8b19ce15f1650ef205bdfe136a7afb5dd5f2
PBS-THE-NOOB <pradiupnashrestha11@gmail.com>
2026-06-11 19:45:56 +0545

    Rock paper sissors game in python

---
+++ 03_ROCK_PAPER_SIZZORS/README.md
@@ -0,0 +1,35 @@
+# Rock Paper Scissors
+
+My first attempt at creating a complete Python game using functions and clean code structure. This project helped me practice program design, user input validation, and game logic.
+
+
+## Features
+
+- Random computer choices
+- User input validation
+- Win, lose, and draw detection
+- Play multiple rounds without restarting the program
+
+## How It Works
+
+1. The player enters either rock, paper, or scissor.
+2. The computer randomly selects a choice.
+3. The program compares both choices.
+4. The result is displayed.
+5. The player can choose to play again or quit.
+
+## Requirements
+
+- Python 3.x
+
+## Running the Program
+
+```bash
+python main.py
+
+## Things I learnt
+
+1. Breaking programs into smaller parts
+2. Keeping logic organized
+3. decomposition( breaking a problem into smaller pieces ) using functions 
+
---
+++ 03_ROCK_PAPER_SIZZORS/main.py
@@ -0,0 +1,41 @@
+import random
+
+def get_computer_choice():
+    computer_to_game_dict={
+        1:"rock",
+        2:"paper",
+        3:"sizzor"
+    }
+    computer=computer_to_game_dict[random.randint(1,3)]
+    return computer
+
+def check_user_choice(user_choice):
+    return user_choice in ["rock","paper","scissor"]
+
+def get_result(computer_choice,user_choice):
+    if user_choice==computer_choice:
+        return "IT IS A DRAW!"
+    elif (user_choice=="rock" and computer_choice=="sizzor") or (user_choice=="paper" and computer_choice=="rock") or (user_choice=="sizzor" and computer_choice=="paper"):
+        return "YOU WIN!"
+    else:
+        return "YOU LOST!"
+
+while True:
+    computer_choice=get_computer_choice()
+    user_choice=input("Enter your choice: ").lower()
+    if check_user_choice(user_choice):
+        print(get_result(computer_choice,user_choice))
+        print(f"You choose:{user_choice}\nComputer choose:{computer_choice}")
+        if check:=input("Do you want to continue(Y/N): ").lower()=="n":
+            break
+    else:
+        print("Invalid Choice")
+
+
+
+
+
+    
+
+
+
